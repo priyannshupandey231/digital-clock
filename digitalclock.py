811757b157c667dc8c6fd1074b0c2f3dcfd2b684
priyanshu <priyanshupandey9118@gmail.com>
2026-09-20 23:13:21 +0530

    this is a digital clock

---
+++ digitalclock.py
@@ -0,0 +1,48 @@
+# python PyQt5 Digital clock
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QTimer, QTime, Qt
+
+
+class DigitalClock(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.time_label = QLabel(self)
+        self.Timer = QTimer(self)
+        self.initUI()
+
+
+
+    def initUI(self):
+        self.setWindowTitle("Digital Clock")
+        self.setGeometry(600, 400, 300, 100) 
+
+
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+        self.setLayout(vbox)
+
+
+        self.time_label.setAlignment(Qt.AlignCenter)
+
+        self.time_label.setStyleSheet("font-size: 150px;"
+                                       "font-family: Arial;"
+                                       "color: #26ff00;")
+        self.setStyleSheet("background-color: black;")
+         
+
+        self.update_time()
+         
+    def update_time(self):
+        current_time = QTime.currentTime().toString("hh:mm:ss AP")
+        self.time_label.setText(current_time)
+        
+
+
+if __name__ == "__main__":
+     app = QApplication(sys.argv)
+     clock = DigitalClock()
+     clock.show()
+     sys.exit(app.exec_())     
